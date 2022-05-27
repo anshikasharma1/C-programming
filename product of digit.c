8a9a7874cc0e6312307e406581f5ba5f83db1385
anshikasharma1 <98009797+anshikasharma1@users.noreply.github.com>
2022-05-27 23:12:22 +0530

    Create product of digit.c

---
+++ product of digit.c
@@ -0,0 +1,17 @@
+// Write a C program to find product of digits of a number.
+#include <stdio.h>
+int main()
+{
+    int num;
+    long long product=1ll;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    product = (num == 0 ? 0 : 1ll);
+    while(num != 0)
+    {
+        product = product * (num % 10);
+        num = num / 10;
+    }
+    printf("Product of digits = %lld", product);
+    return 0;
+}   
