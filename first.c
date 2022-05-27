69ebc76ba141770903afdce9044e1a7725fe2138
anshikasharma1 <98009797+anshikasharma1@users.noreply.github.com>
2022-05-27 22:49:02 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,17 @@
+// Write a C program to find first digit of a number.
+#include <stdio.h>
+int main()
+{
+    int n, first;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+    first = n;
+    while(first >= 10)
+    {
+        first = first / 10;
+    }
+
+    printf("First digit = %d", first);
+
+    return 0;
+}
