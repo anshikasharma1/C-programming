197ba38434c32b38119aab3d14ca87152aeb0507
anshikasharma1 <98009797+anshikasharma1@users.noreply.github.com>
2022-05-27 19:41:36 +0530

    Create week.c

---
+++ week.c
@@ -0,0 +1,39 @@
+// Write a C program to print day of week name using switch case.
+# include<stdio.h>
+int main()
+{
+    int week;
+    printf("Enter week number:");
+    scanf("%d",&week);
+    switch(week)
+    {
+        case-1:
+        printf("Monday");
+        break;
+          
+        case 2:
+        printf("Tuesday");
+        break;
+        
+        case 3:
+        printf("Wednesday");
+        break;
+        
+        case 4:
+        printf("Thursday");
+        break;
+        
+        case 5:
+        printf("Friday");
+        break;
+        
+        case 6:
+        printf("Saturday");
+        break;
+        
+        case 7:
+        printf("Sunday");
+        break;
+    }
+    return 0;
+}
