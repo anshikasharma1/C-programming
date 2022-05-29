ab1e41d3bb55192147322d7257fa97b778cb96d0
anshikasharma1 <98009797+anshikasharma1@users.noreply.github.com>
2022-05-29 22:14:22 +0530

    Create return multiple value.c

---
+++ return multiple value.c
@@ -0,0 +1,25 @@
+// Write a C program to return multiple value from a function using array.
+#include <stdio.h>
+#define SIZE 10
+int * getNEvenNumbers(const int N, int * numbers);
+int main()
+{
+int evenNumbers[SIZE];
+int i;
+getNEvenNumbers(SIZE, evenNumbers);
+printf("First %d even numbers are: \n", SIZE);
+for (i = 0; i < SIZE; i++)
+{
+printf("%d ", *(evenNumbers + i));
+}
+return 0;
+}
+int * getNEvenNumbers(const int N, int * numbers)
+{
+int i;
+for (i = 0; i < N; i++)
+{
+*(numbers + i) = 2 * (i + 1);
+}
+return numbers;
+}
